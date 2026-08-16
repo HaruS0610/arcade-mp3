17a137c9b77e47a830cc1bed26ef2ee75e5bc708
HaruS0610 <h85034588@gmail.com>
2026-08-16 13:21:40 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,14 @@
-//% color="#9C27B0" icon="\uf001" block="MP3"
+//% color="#9C27B0" icon="\uf001" weight=100 block="MP3"
 
 namespace mp3 {
-
     //% block="MP3を再生 %name"
     //% name.shadow="text"
     export function play(name: string): void {
-        // MP3再生処理
+        console.log("MP3: " + name)
     }
 
     //% block="MP3を停止"
     export function stop(): void {
-        // MP3停止処理
+        console.log("MP3 stop")
     }
 }
