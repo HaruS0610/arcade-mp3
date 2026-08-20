edfc8921faa2b3abe158e9e7d3f68ff0dc99d9da
HaruS0610 <h85034588@gmail.com>
2026-08-21 08:01:57 +0900

    Fix MP3 extension

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 //% color="#9C27B0" icon="\uf001" weight=100 block="MP3"
 
 namespace mp3 {
-    //% block="MP3を再生 %name"
+    //% block="Play MP3 %name"
     //% name.shadow="text"
     export function play(name: string): void {
         console.log("MP3: " + name)
     }
 
-    //% block="MP3を停止"
+    //% block="Stop MP3"
     export function stop(): void {
         console.log("MP3 stop")
     }
